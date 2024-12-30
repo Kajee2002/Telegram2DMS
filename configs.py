8613cd50802ccfa575b1e2f510bd5fef621b1251
kajee0311 <kajatheepan0311@gmail.com>
2024-12-31 03:43:40 +0530

    Docker support added

--- configs.py
+++ configs.py
@@ -9,13 +9,9 @@ class Configs(object):
     BOT_TOKEN=os.environ.get("BOT_TOKEN")
     
     #Dms configurations
-    UserName=os.environ.get("UserName")
-    Password=os.environ.get("Password")
-    LoginDetail=f'{os.environ.get("UserName")}:{os.environ.get("Password")}'
     Header = '"OCS-APIRequest: true"'
     UploadPoint = '"https://dms.uom.lk/remote.php/webdav/"'
     SharePoint = '"https://dms.uom.lk/ocs/v2.php/apps/files_sharing/api/v1/shares?format=json"'
     MAX_CONCURRENT_TRANSMISSIONS = 2 # Set the maximum amount of concurrent transmissions (uploads & downloads).
     BOT_UPTIME=time.time()
-    WEB_SUPPORT = bool(os.environ.get("WEB_SUPPORT", "True"))
     DOWNLOAD_PATH='./contains'
